144ea935ef29e79069aa5e0eb0b50c232d6cb002
Ta1ant <149287822+pOL216518@users.noreply.github.com>
2024-11-11 20:47:05 +0800

    add gpt3.5

--- ai/models.ts
+++ ai/models.ts
@@ -8,24 +8,31 @@ export interface Model {
 }
 
 export const models: Array<Model> = [
+  // {
+  //   id: 'gpt-4o-mini',
+  //   label: 'GPT 4o mini',
+  //   apiIdentifier: 'gpt-4o-mini',
+  //   description: 'Small model for fast, lightweight tasks',
+  // },
+  // {
+  //   id: 'gpt-4o',
+  //   label: 'GPT 4o',
+  //   apiIdentifier: 'gpt-4o',
+  //   description: 'For complex, multi-step tasks',
+  // },
+  // {
+  //   id: 'gpt-4',
+  //   label: 'GPT 4',
+  //   apiIdentifier: 'gpt-4',
+  //   description: 'For complex, multi-step tasks',
+  // },
+
   {
-    id: 'gpt-4o-mini',
-    label: 'GPT 4o mini',
-    apiIdentifier: 'gpt-4o-mini',
-    description: 'Small model for fast, lightweight tasks',
-  },
-  {
-    id: 'gpt-4o',
-    label: 'GPT 4o',
-    apiIdentifier: 'gpt-4o',
-    description: 'For complex, multi-step tasks',
-  },
-  {
-    id: 'gpt-4',
-    label: 'GPT 4',
-    apiIdentifier: 'gpt-4',
+    id: 'gpt-3.5-turbo',
+    label: 'Gpt 3.5 turbo',
+    apiIdentifier: 'gpt-3.5-turbo',
     description: 'For complex, multi-step tasks',
   },
 ] as const;
 
-export const DEFAULT_MODEL_NAME: string = 'gpt-4o-mini';
+export const DEFAULT_MODEL_NAME: string = 'gpt-3.5-turbo';
